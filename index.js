4a4c6cd00946ab14c733ae8a2db25fdb409695a5
GIngleton <georgeingleton@gmail.com>
2017-05-17 07:59:38 -0400

    make it better

---
+++ index.js
@@ -0,0 +1,22 @@
+var htttp = require('http');
+var express = require('express');
+var ejs = require('ejs');
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+  res.render("home");
+});
+
+app.get("/login", function(req, res){
+  res.render('login');
+});
+
+app.get("/signup", function(req, res){
+  res.render('signup');
+});
+
+
+app.listen(8080, function(){
+  console.log("Server is running");
+});
